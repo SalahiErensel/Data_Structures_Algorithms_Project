59948dd6421dde258e260b61b98254670634b379
Salahi Erensel <salahi_erensel@hotmail.com>
2024-08-28 18:01:55 +0300

    Added dynamic arrays with both default and custom class usages

---
+++ DynamicArrays/DynamicArray.cs
@@ -0,0 +1,149 @@
+﻿namespace DynamicArrays
+{
+    public class DynamicArray
+    {
+        public int size;
+        //Default
+        public int capacity = 10;
+
+        Object[] array;
+
+        //Creating default 10 capacity array
+        public DynamicArray()
+        {
+            this.array = new Object[capacity];
+        }
+
+        //Creating array with given capacity
+        public DynamicArray(int capacity)
+        {
+            this.capacity = capacity;
+            this.array = new Object[capacity];
+        }
+
+        //Adding element to the array
+        public void Add(Object data)
+        {
+            if(size >= capacity)
+            {
+                Grow();
+            }
+            array[size] = data;
+            size++;
+        }
+
+        //Inserting element to the array with index and element data
+        public void Insert(int index, Object data)
+        {
+            if(size >= capacity)
+            {
+                Grow();
+            }
+
+            //Moving element to right to insertion
+            for(int i = size; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[index] = data;
+            size++;
+        }
+
+        //Deleting element from the array
+        public void Delete(Object data)
+        {
+            //Moving element to left to deletion
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i] == data)
+                {
+                    for (int j = 0; j < (size - i - 1); j++)
+                    {
+                        array[i + j] = array[i + j + 1];
+                    }
+
+                    array[size -1] = null;
+                    size--;
+                    if(size <= (int) (capacity / 3)){
+                        Shrink();
+                    }
+                    
+                    break;
+                }
+            }
+        }
+
+        //Searching element in the array
+        public int Search(Object data)
+        {
+            for(int i = 0; i < size; i++)
+            {
+                if (array[i] == data)
+                {
+                    return i;
+                }
+            }
+
+            //if value is not in the array
+            return -1;
+        }
+
+        //Making array bigger
+        private void Grow()
+        {
+            int newCapacity = (int) capacity * 2;
+            Object[] newArray = new Object[newCapacity];
+
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+            capacity = newCapacity;
+            array = newArray;
+        }
+
+        //Making array smaller for excess memory usage and efficiency
+        private void Shrink()
+        {
+            int newCapacity = (int)capacity / 2;
+            Object[] newArray = new Object[newCapacity];
+
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+            capacity = newCapacity;
+            array = newArray;
+        }
+
+        //checking if array is empty
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        //Returning elements in the array
+        public String toString()
+        {
+            String str = "";
+
+            for(int i = 0; i < size; i++)
+            {
+                str += array[i] + ", ";
+            }
+
+            if(str != "")
+            {
+                str = "[" + str.Substring(0, str.Length - 2) + "]";
+            }
+
+            else
+            {
+                str = "[]";
+            }
+
+            return str;
+        }
+    }
+}
---
+++ DynamicArrays/Program.cs
@@ -0,0 +1,91 @@
+﻿using DynamicArrays;
+using System.Collections;
+
+public class DynamicArraysApp
+{
+    public static void Main(string[] args)
+    {
+        //Creating a dynamic array
+        ArrayList dynamicArray = new ArrayList();
+
+        //Adding elements to the dynamic array
+        dynamicArray.Add("Food");
+        dynamicArray.Add("Sleep");
+        dynamicArray.Add("Coding");
+        dynamicArray.Add("Gaming");
+
+        //First Element is Food, last element is Gaming
+        Console.WriteLine("First of the dynamic array: " + dynamicArray[0]);
+        Console.WriteLine("\nLast of the dynamic array: " + dynamicArray[dynamicArray.Count - 1]);
+        Console.WriteLine("\nDynamic array count: " + dynamicArray.Count);
+
+        //Removing the first element from dynamic array
+        dynamicArray.RemoveAt(0);
+
+        Console.WriteLine("\nFirst of the dynamic array after remove first: " + dynamicArray[0]);
+        Console.WriteLine("\nDynamic array count after remove first: " + dynamicArray.Count);
+
+        //Removing the last element from dynamic array
+        dynamicArray.RemoveAt(dynamicArray.Count - 1);
+
+        Console.WriteLine("\nLast of the dynamic array after remove last: " + dynamicArray[dynamicArray.Count - 1]);
+        Console.WriteLine("\nDynamic array count after remove last: " + dynamicArray.Count);
+
+        //Removing the element "Sleep" from dynamic array
+        dynamicArray.Remove("Sleep");
+
+        Console.WriteLine("\nDynamic array count after remove 'Sleep': " + dynamicArray.Count);
+
+        //Inserts at the first index
+        dynamicArray.Insert(0, "Sleep");
+
+        //Inserts at the last index
+        dynamicArray.Insert(dynamicArray.Count, "Gaming");
+
+        Console.WriteLine("\nDynamic array last index  after adding 'Gaming': " + dynamicArray.Count);
+
+        //Finds the index of first coding element
+        Console.WriteLine("\nIndex of Coding element: " + dynamicArray.IndexOf("Coding"));
+
+        //Finds the index of last coding element
+        dynamicArray.LastIndexOf("Coding");
+
+        //Clearing the dynamic array
+        dynamicArray.Clear();
+
+
+
+        //*******************************************************************************************************************************************************************//
+
+        //Another way of creating dynamic arrays by your own class. use DynamicArray class
+
+        //Creating a dynamic array with capacity 5
+        DynamicArray dynamicArray2 = new DynamicArray(5);
+
+        //Adding to array
+        dynamicArray2.Add("A");
+        dynamicArray2.Add("B");
+        dynamicArray2.Add("C");
+        dynamicArray2.Add("D");
+        dynamicArray2.Add("E");
+        dynamicArray2.Add("F");
+
+        //Inserting at index 0 element x
+        dynamicArray2.Insert(0, "X");
+
+        //Deleting element 
+        dynamicArray2.Delete("A");
+        dynamicArray2.Delete("B");
+        dynamicArray2.Delete("C");
+
+        Console.WriteLine("\nSearching X in Array: " + dynamicArray2.Search("X"));
+
+        Console.WriteLine("\nSearching A in Array after deleting: " + dynamicArray2.Search("A"));
+
+        Console.WriteLine("\nSecond Dynamic Array Elements: " + dynamicArray2.toString());
+
+        Console.WriteLine("\nSecond Dynamic Array Is Empty: " + dynamicArray2.isEmpty());
+        Console.WriteLine("\nSecond Dynamic Array Size: " + dynamicArray2.size);
+        Console.WriteLine("\nSecond Dynamic Array Capacity: " + dynamicArray2.capacity);
+    }
+}

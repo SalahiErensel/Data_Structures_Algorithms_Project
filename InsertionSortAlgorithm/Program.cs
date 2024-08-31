ba983b020b8e8828a901bd4f9bede4a66c5b372c
Salahi Erensel <salahi_erensel@hotmail.com>
2024-08-31 20:15:31 +0300

    Insertion Sort Algorithm Added.

---
+++ InsertionSortAlgorithm/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+After comparing elements to the left, shift elements to the right to make a room to insert a value.
+Run time complexity = O(n2).
+Goo for small data sets, bad for large data sets. Less steps than bubble sort, best case is O(n) compared to Selection Sort O(n2)
+*/
+
+public class InsertionSortAlgorithmApp
+{
+    public static void Main(string[] args)
+    {
+        //Creating integer array
+        int[] array = { 8, 7, 9, 2, 3, 1, 5, 4, 6 };
+
+        //Sorting array
+        InsertionSort(array);
+
+        //Showing sorted array
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    private static void InsertionSort(int[] array)
+    {
+        for(int i = 1; i < array.Length; i++)
+        {
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = temp;
+        }
+    }
+}
---
+++ SelectionSortAlgorithm/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ Search through the array and keep track of minimum value during each iteration. At the end of each iteration, swap variables.
+ */
+
+public class SelectionSortAlgorithmApp
+{
+    public static void Main(string[] args)
+    {
+        //Creating integer array
+        int[] array = { 8, 7, 9, 2, 3, 1, 5, 4, 6 };
+
+        //Sorting array
+        SelectionSort(array);
+
+        //Showing sorted array
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    private static void SelectionSort(int[] array)
+    {
+        //Iterating through array
+        for (int i = 0; i < array.Length - 1; i++)
+        {           
+            int min = i;
+
+            for (int j = i + 1; j<array.Length; j++)
+            {
+                //For descending order, change the sign to '<'
+                if (array[min] > array[j])
+                {
+                    min = j;
+                }
+            }
+
+            //Swapping
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+}
